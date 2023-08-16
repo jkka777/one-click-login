706cb8915bb6e4438d3a91220a2a3d18acb3369b
jkka777 <abhiram.kishore@gmail.com>
2023-08-16 11:39:29 +0530

    updated changes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// Load the Facebook SDK asynchronously
-window.fbAsyncInit = function () {
+
+window.fbAsyncInit = () => {
     FB.init({
         appId: '198579683010383',
         cookie: true,
         xfbml: true,
         version: 'v17.0'
+    });
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+        console.log(response);
     });
 };
 
@@ -17,7 +22,7 @@ window.fbAsyncInit = function () {
 }(document, 'script', 'facebook-jssdk'));
 
 // Gmail Login Button Click Event
-document.getElementById('gmailLogin').addEventListener('click', function () {
+document.getElementById('gmailLogin').addEventListener('click', () => {
 
     const gmailAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
         'client_id=113981145395-h9hhgokpqmumieih7g63pbc74a41pahc.apps.googleusercontent.com' +
@@ -28,16 +33,26 @@ document.getElementById('gmailLogin').addEventListener('click', function () {
 });
 
 // Facebook Login Button Click Event
-document.getElementById('facebookLogin').addEventListener('click', function () {
+document.getElementById('facebookLogin').addEventListener('click', () => {
     FB.login(function (response) {
         if (response.authResponse) {
+
+            const accessToken = response.authResponse.accessToken;
+
+            //getting profile info using access token
+            FB.api('/me', { fields: 'id,name,email' }, function (profileResponse) {
+                if (profileResponse && !profileResponse.error) {
+                    const userId = profileResponse.id;
+                    const userName = profileResponse.name;
+                    const userEmail = profileResponse.email;
+
+                    window.open('https://one-click-login.vercel.app/main.html', '_blank');
+                }
+            });
 
         } else {
             console.log('User cancelled login or did not fully authorize.');
         }
     });
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-        console.log(response);
-    });
+
 });
